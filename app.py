8b041073595344ea9287cb06b54c5eccbd3421e7
lcp020620 <lcp020620@kaist.ac.kr>
2026-01-22 23:02:32 +0900

    ..

--- app.py
+++ app.py
@@ -111,7 +111,7 @@ def resetData():
 def shutdown():
     print("shutdown recieved")
     os.kill(os.getpid(), signal.SIGINT)
-    return 'Server shutting down...'
+    return render_template('goodbye.html')
 
 def open_browser():
     # webbrowser.open_new("http://127.0.0.1:5000")
